25b1cfcfba9f6b7727e7b99c345f119a7a45fb1c
heesk0223@gmail.com <heesk0223@gmail.com>
2025-03-16 23:50:00 +0900

    commit

---
+++ src/electron/main.ts
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { isDev } from './util.js';
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({});
+  if (isDev()) {
+    // npm run dev:react를 한 상태에서 npm run dev:electron을 사용해야함
+    mainWindow.loadURL('http://localhost:5123');
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath() + '/dist-react/index.html'));
+  }
+});
